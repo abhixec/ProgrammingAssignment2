6f5bdbdedd2ef8ba4ebcd7ef735df673bffa9374
abhinav <abhinav19g@gmail.com>
2015-08-21 22:51:19 -0700

    Solution for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Author: datavector
+# Programming Assignment #2
+#Problem Statement:
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# Contains the  setters and getters for the matrix
+# and also for the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <-  function(y){
+    x<<- y
+    inverse<<-NULL
+  }
+  get <- function() x
+  setInverse <- function(inverseMatrix) inverse<<- inverseMatrix
+  getInverse <- function() inverse
+  list(set=set,get=get,setinverse=setInverse,getinverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
+#---------------------------------------------------------------------------------------------------------------
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+#retrieve the inverse from the cache.
+#---------------------------------------------------------------------------------------------------------------
+# Checks if the inverse of the matrix has already been computed
+# if so prints the cached value 
+# if not it computes the inverse of the matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inverseMatrix<-x$getinverse()
+      print (inverseMatrix)
+      if( !is.null(inverseMatrix)){
+        print("Returning the cached inverse matrix")
+        return(inverseMatrix)
+      }
+      inputMatrix<-x$get()
+      inverseMatrix<-solve(inputMatrix,...)
+      x$set(inverseMatrix)
+      inverseMatrix
 }
+
+# Test the code
+#input<- matrix(c(7,0,2,2,3,4,1,-1,-2), 3, 3)
+#class(input)
+#matrix<-makeCacheMatrix(input)
+#matrix$get()
+#cacheSolve(matrix)
